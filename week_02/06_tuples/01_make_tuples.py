709c042c81627b786b3cc7c2e73785912da202f7
Mingyyy <j.yanming@gmail.com>
2019-02-25 18:13:30 +0800

    first commit for week_02

--- week_02/06_tuples/01_make_tuples.py
+++ week_02/06_tuples/01_make_tuples.py
@@ -11,4 +11,30 @@ If the user enters an odd numbered list, add the last item
 to a tuple with the number 0.
 
 '''
+try:
+    number_list = input("Please enter some numbers and separate them by comma ',':").split(",")
+    print(number_list)
+
+except TypeError:
+    print("Please make sure you enter only numbers")
+
+if len((number_list)) % 2 != 0:
+    number_list.append(0)
+
+new_list = []
+for i in number_list:
+    new_list.append(float(i))
+
+new_list.sort()
+print(new_list)
+result = []
+for i in range(int(len(new_list)/2)):
+    tup = []
+    for j in range(2):
+        tup.append(new_list[j + i * 2])
+    result.append(tuple(tup))
+
+print(result)
+
+
 
--- week_02/06_tuples/02_word_frequencies.py
+++ week_02/06_tuples/02_word_frequencies.py
@@ -12,3 +12,32 @@ l, h, e, o
 For letters that are the same frequency, the order does not matter.
 
 '''
+
+
+def most_frequent(user_str):
+    ''' This funciton takes in one variable: a string and returns the letters in descending order of frequency
+    :param any string
+    :result ordered based on frequency
+    '''
+
+
+    temp = list(set(user_str))
+    counter = []
+    for i in range(len(temp)):
+        temp[i]= str(user_str.count(temp[i])) + temp[i]
+
+    temp.sort()
+    temp.reverse()
+
+    for i in range(len(temp)):
+        temp[i] = temp[i][-1:]
+
+    result = ""
+    for i in temp:
+        result += (i + ", ")
+    result = result.rstrip(", ")
+
+    print(result)
+
+most_frequent("hello")
+most_frequent("ana")
