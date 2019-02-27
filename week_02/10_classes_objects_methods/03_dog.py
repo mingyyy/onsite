2fefc3a5478fb7434b196997ea20b6a86f3d97ea
Mingyyy <j.yanming@gmail.com>
2019-02-27 20:32:22 +0800

    second commit Wed, Week2

--- week_02/10_classes_objects_methods/03_dog.py
+++ week_02/10_classes_objects_methods/03_dog.py
@@ -24,7 +24,55 @@ Building on the dog exercise in the previous section:
 6. Create at least two objects of the dog class to demonstrate the functionality.
 
 
-
-
 '''
+
+
+class dog:
+    def __init__(self, name, color, age, is_hungry=False, percent_full=100):
+        """
+        This dog class defines some aspects of the dog.
+        :param name: the name of the dog in string
+        :param color: color of the dog in string
+        :param age: a number in years
+        :param is_hungry: optional, default as False
+        :param percent_full: a number from 0 to 100; optional, default 100.
+        """
+        self.name = name
+        self.color = color
+        self.age = age
+        self.is_hungry = is_hungry
+        self.percent_full = percent_full
+
+    def str(self):
+        if self.is_hungry:
+            s = "hungry"
+        else:
+            s = "not hungry"
+        if self.age > 1:
+            return print(f"{self.name} is a {self.age} years old {self.color} dog. "
+                     f"It's {s}, {self.percent_full}% full.")
+        elif self.age == 1:
+            return print(f"{self.name} is a {self.age} year old {self.color} dog. "
+                     f"It's {s}, {self.percent_full}% full.")
+        else:
+            return print(f"{self.name} is a {int(self.age*12)}-month old {self.color} dog. "
+                     f"It's {s}, {self.percent_full}% full.")
+
+    def sleep(self):
+        self.is_hungry = True
+        self.percent_full = 20
+        return print(f"The dog is hungry! Only {20}% full!")
+
+    def eat(self):
+        self.is_hungry = False
+        self.percent_full = 100
+        return print(f"The dog is not hungry at all! ")
+
+if __name__ == '__main__':
+
+    lucy = dog("Lucy","black and white", 5, False, 80)
+    lucy.sleep()
+    lucy.str()
+    lucy.eat()
+
 
--- week_02/mini_projects/guess.py
+++ week_02/mini_projects/guess.py
@@ -9,3 +9,15 @@ pick the correct (randomly generated) number between 1 and 100.
 Tip: Use python's 'random' module.
 
 '''
+import random
+flag = True
+guess = 100
+while flag:
+    user_input = input(f"Please guess a number between 1 and {guess}: ")
+    ans = random.randint(1, guess)
+    counter = 0
+    if int(user_input) == ans:
+        print("You won!")
+        flag = False
+    else:
+        continue
