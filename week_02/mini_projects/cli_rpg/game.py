4b937dfe86562ddeb5db4cc90cf2057175a42cc7
Mingyyy <j.yanming@gmail.com>
2019-03-03 15:47:52 +0800

    version 1 of RPG game.

--- week_02/mini_projects/cli_rpg/game.py
+++ week_02/mini_projects/cli_rpg/game.py
@@ -1,7 +1,7 @@
 
-import random
+import random as rd
 import datetime as dt
-from roles import Hero, Sidekick, Alien
+from roles import Hero, Sidekick, Alien, Sidekick_effect
 
 
 def main():
@@ -27,13 +27,115 @@ magical pills which can transform you to the hero we need!
 Unfortunately your friend went missing before he could tell
 you which pill does what...
 
-You have received three bottles in a parcel sent to outpost,
-First one read: Fisib
-Second one says: Musbit
-The last one shows: Drog
+You have received three bottles in a parcel sent to Outpost,
+1. First one read: Fisib
+2. Second one says: Musbit
+3. The last one shows: Drog
 ....................The choice is your....................
 """)
 
 
 def play():
-    pass
+    # define the Hero.
+    user_input = input("Now tell us your choice please ('Fisib','Musbit','Drog'): ")
+    you = Hero(user_input)
+    print(you)
+    print("""
+    --------------------------------------------------------------
+    You need more time to adapt to your newly gained powers...
+    But, there is NO TIME!!!
+    Alien is approaching Nyuh Kuning, the heart of Codingnomads...
+    --------------------------------------------------------------
+    """)
+
+    # define the Alien
+    user_input = input("Rumor says, the alien is called: ")
+    alien = Alien(user_input)
+    print(alien)
+
+    find_weakness = False
+    sidekick_here = False
+    while int(alien.IQ / 10) + int(alien.energy) > int(you.power) and find_weakness is False:
+        print("""
+            --------------------------------------------------------------
+            Now, Alien is here!!!!!
+            You have to do something. What are you going to choose?
+            1. Attack
+            2. Run away
+            3. Find some help
+            --------------------------------------------------------------
+            """)
+        user_input = input("Enter your choice (1 for Attack, 2 for Run, 3 for help) ")
+        if user_input == "1":
+            you.power /= max(rd.choice([.05, 0.1, 0.5, 1, 1,2, 1,5, 2, 3, 4]),0)
+            alien.energy = max(alien.energy+rd.randint(-5, 10), 0)
+            if sidekick_here is True:
+                Sidekick_effect(alien, helper)
+                print("With the help of your sidekick...")
+                print(Sidekick_effect(alien, helper))
+                print(f"You, on the other hand, have power of {int(you.power)}. ")
+            else:
+                # display the status of you and your enemy
+                print(f"Alien now has and of IQ {int(alien.IQ)} and energy level of {int(alien.energy)}, "
+                      f"while you have power of {int(you.power)}.")
+            if int(alien.IQ / 10) + int(alien.energy) > int(you.power) >= int(alien.energy):
+                print(f"You are kicking his ass! but the Alien is very smart with an IQ of {int(alien.IQ)}.")
+            elif int(you.power) < int(alien.energy):
+                print("Ohhhh, Noooooo.........\nYou are not his match right now! Sorry...You can try again though.")
+                continue
+
+        elif user_input == "2":
+            alien.energy += 2
+            print("Alien is getting stronger by the minute!")
+            if rd.randint(1, 4) == 4:
+                you.power = max(you.power * 2, + 10)
+                print("However, you have rested and re-gained some power!")
+        elif user_input == "3":
+            alien.energy += 1
+            print("Alien is getting stronger by the day!")
+            # sidekick or weakness
+            print("""
+            --------------------------------------------------------------
+            On your way to your friends in the Holy Spirited Monkey Forest, 
+            You saved a dying spy. With his last breath, he told you that 
+                           "The alien has ONE WEAKNESS."
+            
+            Now you have to decide whether you want to:
+                  "go find a sidekick" Or "find the weakness alone"
+            --------------------------------------------------------------
+            """)
+            user_input = input("Please enter your choice (1 for sidekick, 2 for weakness): ")
+            if user_input == "1":
+                name = input("Please enter the name of your sidekick: ")
+                func = input("Please enter the ability of your sidekick: ")
+                helper = Sidekick(name, func)
+                print(helper)
+                sidekick_here = True
+                print(f"From now on, you will be accompanied by your friend {name}! Bravo!")
+            elif user_input == "2":
+                print("Your bravery just gained you a chance to destroy him for good!")
+                user_input = input("Pleaes give me a number from 1 to 10: ")
+                if alien.weakness == int(user_input):
+                    print("Wowowow! You have found the weakness!")
+                    find_weakness = True
+                else:
+                    print("Sorry, maybe you will find it next time!")
+            else:
+                print("I see you have decided to move on from this opportunity...")
+                alien.energy += 1
+                print("Alien is getting stronger by the day!")
+        elif user_input == "q":
+            find_weakness = True
+        else:
+            print("You are the chosen one! You have to do something! Luckily, "
+                  "you have another chance before it's too late!")
+    print("Congratulations! You have defeated the alien and saved the land of Codingnomads!")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
--- week_02/mini_projects/cli_rpg/roles.py
+++ week_02/mini_projects/cli_rpg/roles.py
@@ -1,22 +1,30 @@
 import random as rd
 import math as m
 
-hero_list = {"Fisib": "You can fly and be invisible.", "Musbit":
-    "You have superb physical strength and hacker skills",
-             "Drog": "You can turn to a fire dragon with invisible skins."}
+hero_list = {"Fisib": "you can fly and be invisible.", "Musbit":
+             "you have superb physical strength and hacker skills.",
+             "Drog": "you can turn to a invisible fire dragon."}
 
 
 class Hero:
     '''Defines the role of Heros'''
+    power = 10
+
     def __init__(self, user_input="Fisib"):
         self.user_input = user_input
         x=""
         if self.user_input.capitalize() not in ["Fisib", "Musbit", "Drog"]:
             self.user_input = "Fisib"
         self.x = hero_list[self.user_input.capitalize()]
+        if self.user_input.capitalize() == "Fisib":
+            self.power += 3
+        elif self.user_input.capitalize() == "Musbit":
+            self.power += rd.randint(1, 10)
+        else:
+            self.power += 6
 
     def __repr__(self):
-        return f"You are our hero: {self.user_input} now! {self.x}"
+        return f"You are our hero {self.user_input}! You suddenly find that, {self.x}"
 
 
 class Sidekick:
@@ -29,18 +37,21 @@ class Sidekick:
         return f'You sidekick is {self.nickname} who can {self.function}.'
 
 
-def Sidekick_effect(hero, side):
+def Sidekick_effect(alien, side):
     '''Testing what effect the sidekick will have on the hero
     inputs: hero instance, sidekick instance
     '''
-    if 20 >len(Sidekick.function) > 10:
-        Alien.IQ /= 2
-    elif len(Sidekick.function)< 5:
-        Alien.energy = m.log10(Alien.energy)
-    elif 5 < len(Sidekick.function) < 10:
-        Alien.energy = m.log2(Alien.energy)
-    elif len(Sidekick.function) == 5 or len(Sidekick.function) ==10:
-        Alien.IQ /=10
+    if 20 >len(side.function) > 10:
+        alien.IQ /= 2
+    elif len(side.function)< 5:
+        alien.energy = m.log10(max(alien.energy, 1))
+    elif 5 < len(side.function) < 10:
+        alien.energy = m.log2(max(alien.energy, 1))
+    elif len(side.function) == 5 or len(side.function) ==10:
+        alien.IQ /= 10
+    else:
+        alien.IQ /= 3
+    return f"Alien now has an IQ of {int(alien.IQ)} and energey level of {int(alien.energy)}. "
 
 
 class Alien:
@@ -48,30 +59,28 @@ class Alien:
     IQ_level = []
     power_level = []
     energy_level = []
-    weakness_list = ["scorpio", "gelato", "tempeh", "water", "coding", "nothing"]
+    #weakness_list = ["scorpio", "gelato", "tempeh", "water", "coding", "nothing"]
     x = 10 #complexity
 
-
-    def __init__(self, name, energy=100, IQ=1000, weakness="nothing"):
+    def __init__(self, name, energy=100, IQ=1000, weakness=0):
         self.name = name
         self.energy = energy
         self.IQ = IQ
         self.weakness = weakness
 
-        self.weakness_list = rd.choice(self.weakness_list)
         self.energy = rd.choice(self.energy_level)
         self.IQ = rd.choice(self.IQ_level)
-        self.weakness = rd.choice(self.weakness_list)
+        self.weakness = rd.randint(1, 10)
 
-    for i in range(x): # define the options for different combinations
-        IQ_level.append((i+1) * (x**2))
-        energy_level.append(m.pi ** (m.sqrt(x)))
+    # define the options for different combinations
+    i = rd.randint(1, 10)
+    IQ_level.append((i+1) * (x**2))
+    energy_level.append(m.pi ** (m.sqrt(i)))
 
     def __str__(self):
 
-        return f'{self.name} is an alien with an IQ level of {int(self.IQ)}, ' \
-            f'and energy level of {int(self.energy)}, ' \
-            f'who is afraid of {self.weakness}.'
+        return f'{self.name.capitalize()} is an alien with an IQ level of {int(self.IQ)}, ' \
+            f'and energy level of {int(self.energy)}. Also very ugly!'
 
 
 # subclasses
@@ -101,3 +110,6 @@ print(mel)
 
 seb = Sidekick("Sadie","vanish into thin air")
 print(seb)
+
+print(Sidekick_effect(martin, seb))
+
