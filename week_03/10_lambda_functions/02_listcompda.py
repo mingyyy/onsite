e6c67e05c16d51c9bb78faa043f754ed5d886f2b
Mingyyy <j.yanming@gmail.com>
2019-03-10 13:00:22 +0800

    one more way for week_03/10_lambda_functions/02_listcompda.py

--- week_03/10_lambda_functions/02_listcompda.py
+++ week_03/10_lambda_functions/02_listcompda.py
@@ -6,6 +6,9 @@ Re-write the following listcomp as a lambda expression.
 names = ['Anne', 'Amy', 'Bob', 'David', 'Carrie', 'Barbara', 'Zach']
 print([x.startswith('D') for x in names])
 
-get_d = lambda x: x.startswith('D')
-for i in names:
-    print(get_d(i))
+# get_d = lambda x: x.startswith('D')
+# for i in names:
+#     print(get_d(i))
+
+print(list(map(lambda x: x.startswith('D'), names)))
+
