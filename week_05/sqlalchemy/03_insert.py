5f404c323f56dd3107b5a6a4b7089077d8f4b51f
Mingyyy <j.yanming@gmail.com>
2019-03-21 18:35:38 +0800

    adding week_05/sqlalchemy 03 and 04

--- week_05/sqlalchemy/03_insert.py
+++ week_05/sqlalchemy/03_insert.py
@@ -2,3 +2,27 @@
 Insert a new record in the film table.
 
 '''
+
+import sqlalchemy as sqa
+from pprint import pprint
+
+username = "Ming"
+password = ""
+db_name = "dvdrental"
+# connection string to postgres
+DATABASE_URI = f"postgres+psycopg2://{username}@localhost:5432/{db_name}"
+engine = sqa.create_engine(DATABASE_URI, echo=True)
+connection = engine.connect()
+metadata = sqa.MetaData()
+
+table_name = 'film'
+t = sqa.Table(table_name, metadata, autoload=True, autoload_with=engine)
+# Print full table metadata
+pprint(repr(t))
+
+query = sqa.insert(t).values(film_id=10000, title="coco", description="cartoon",release_year=2017, language_id=1,
+rental_duration=3, rental_rate=4.5, length=130, replacement_cost=19.99, rating='PG-13',
+last_update='2018-05-26 14:50:58.951', special_features="Trailers, Commentaries")
+# ::tsvector
+
+result = connection.execute(query)
--- week_05/sqlalchemy/04_update.py
+++ week_05/sqlalchemy/04_update.py
@@ -3,3 +3,22 @@ Update all films in the film table to a rental_duration value of 10,
 in the length of the movie is more than 150.
 
 '''
+import sqlalchemy as db
+from pprint import pprint
+
+username = "Ming"
+password = ""
+db_name = "dvdrental"
+# connection string to postgres
+DATABASE_URI = f"postgres+psycopg2://{username}@localhost:5432/{db_name}"
+engine = db.create_engine(DATABASE_URI, echo=True)
+conn = engine.connect()
+metadata = db.MetaData()
+
+table_name = 'film'
+t = db.Table(table_name, metadata, autoload=True, autoload_with=engine)
+
+# UPDATE film SET rental_duration = 10 WHERE length = 100;
+# in total 12 records and rental_duration from 3-6
+query = db.update(t).values(rental_duration=10).where(t.columns.length == 100)
+result = conn.execute(query)
