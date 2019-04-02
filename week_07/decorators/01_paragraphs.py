2d0a52e09c7fc2e1fa8c11ec814c7c91a356fac0
Mingyyy <j.yanming@gmail.com>
2019-04-02 19:55:56 +0800

    adding decorators homework

--- week_07/decorators/01_paragraphs.py
+++ week_07/decorators/01_paragraphs.py
@@ -2,3 +2,16 @@
 Write a decorator function that wraps text passed to it in <p> tags.
 
 '''
+
+
+def p_tagging(func):
+    ''' wrap text in func in <p></p> tags'''
+    def wrapper(*args, **kwargs):
+        return f'<p>{func(*args, **kwargs)}</p>'
+    return wrapper
+
+@p_tagging
+def func(name):
+    return f"{name} is in bali"
+
+print(func("martin"))
--- week_07/decorators/02_tagging.py
+++ week_07/decorators/02_tagging.py
@@ -3,3 +3,16 @@ Improve the decorator from the previous exercise by allowing it to take
 a tag as an input - making it more general.
 
 '''
+def p_tagging(func):
+    ''' wrap text in func in <p></p> tags'''
+    def wrapper(*args, **kwargs):
+        for x in args:
+            if x in ["p","q","div","h1", "h2"]:
+                return f'<{x}>{func(*args, **kwargs)}</{x}>'
+    return wrapper
+
+@p_tagging
+def func(name, tag):
+    return f"In {tag} tag: {name} is in bali!"
+
+print(func("martin","div"))
